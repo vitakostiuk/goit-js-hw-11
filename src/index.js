8b0a183850c115e87aec152be177e412ad4e0d8e
Vita Kostiuk <vitagrebennik@gmail.com>
2022-02-18 13:03:19 +0200

    add smooth scroll

--- src/index.js
+++ src/index.js
@@ -73,6 +73,7 @@ function onLoadMore() {
       imageApiService.incrementPage();
 
       renderImages(data.hits);
+      smoothScroll();
       openLargeImage().refresh();
       
       if (imageApiService.page * imageApiService.perPage >= data.totalHits) {
@@ -98,4 +99,15 @@ function openLargeImage() {
     captionDelay: 250,
   });
   return lightbox;
+}
+
+function smoothScroll () {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
 }
